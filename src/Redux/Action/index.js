0bd088738263b764dcf9aba8b8f24be6998b186d
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-09-01 10:37:36 +0530

    Create index.js

---
+++ src/Redux/Action/index.js
@@ -0,0 +1,14 @@
+export const
+    setInputTitle= (title) => ({
+      type: "SET_INPUT_TITLE",
+      title,
+    }) 
+    export const
+    createBlog= (blog) => ({
+      type: "CREATE_BLOG",
+      blog,
+    }) 
+    export const
+    setReset= () => ({
+      type: "RESET_INPUT"
+    })
