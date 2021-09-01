b71f713f3e6225ca2a7e85efd7d26eda652b5d9c
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-09-01 10:34:43 +0530

    Create registration.js

---
+++ src/Components/Login/registration.js
@@ -0,0 +1,18 @@
+import React from "react";
+const register =()=>
+{
+
+return(
+<div className="register">
+<h1>REGISTRATION</h1>
+<label>Name</label> <br />
+<input type="text" placeholder="ENter your name" /><br />
+<label>Phone No.</label> <br />
+<input type="number" placeholder="ENter your Phone No." /><br />
+<label>Create Password</label><br />
+<input type="Password" placeholder="Password" /><br />
+<button id="btn">register</button>
+
+</div>
+)}
+export default register;
