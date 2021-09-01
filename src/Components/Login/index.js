004ae53018d1877a7e787430017a6c81c39d3cab
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-09-01 10:33:52 +0530

    Create index.js

---
+++ src/Components/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Login =()=>
+{
+
+return( 
+    <div className="login" >
+ <input type="text" placeholder="User Id"/>
+ <input type="password" placeholder="Password"/>
+ <button id="btnn" >login</button>
+
+ <p>Have you registered?  </p>
+    </div>
+)}
+export default Login;
