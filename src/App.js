30372aa7c75b9466a3f19244ceecdf10b2def1dd
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-09-01 10:40:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-// import { setInputTitle, createBlog, setReset } from "../src/Redux/Action/index";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useState } from "react";
+
 import './App.css';
-import Navbar from "./Navbars";
-import Side from "./Side"
+import Navbar from "../Components/Navbars";
+import Side from "../Components/Side"
 import Header from "./components/Header";
 import Main from "./components/Main"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"; 
