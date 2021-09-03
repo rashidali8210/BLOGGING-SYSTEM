5a98984823d08722bdac85150c6beea51cb163bf
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-09-03 12:08:28 +0530

    Update app.js

--- MernBackend/src/app.js
+++ MernBackend/src/app.js
@@ -3,14 +3,11 @@ const bodyParser = require('body-Parser');
 const path = require('path');
 const app=express();
 require("./db/connection");
-
 const port = process.env.PORT || 3000;
+const doten=require("dotenv');
+doten.configuration({path: "./configuration.env"});
+const DB=process.env.DATABASE;
 
-
-
-
-// app.use(bodyParser.Urlencoded({extended:true}));
-// app.post('./example',(req,res))
 app.get("/", (req,res)=>
 {
   res.send("connected")
@@ -19,5 +16,19 @@ app.get("/Home", (req,res)=>
 {
   res.send("login")
 });
+
+app.post('/' , async (req,res)=>
+{  
+    res.send(`${req.body.fname}`); 
+    const registrEmpolye=new register (
+        {
+            name: req.body.name ,
+          number: req.body.number,
+          password: req.body.{password}
+        }
+    )
+    const resti= await registrEmpolye.save();
+    res.status(201).render(index);
+
 app.listen(port,()=>{
   console.log('successful  '); })
